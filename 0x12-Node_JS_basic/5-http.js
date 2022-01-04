4c6f591637ec5b644577a3e39f4647c09215b7df
Wilder Rincon <1588@holbertonschool.com>
2022-01-03 20:33:29 -0500

    Solutiion error task 6

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,36 +1,23 @@
 const http = require('http');
-
-const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[0];
-
-const hostname = '127.0.0.1';
-const port = 1245;
-
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  const { url } = req;
-
-  if (url === '/') {
-    res.write('Hello Holberton School!');
-  } else if (url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
+const app = http
+  .createServer((req, res) => {
+    console.log(process.argv);
+    switch (req.url) {
+      case '/':
+        res.end('Hello Holberton School!');
+        break;
+      case '/students':
+        res.write('This is the list of our students\n');
+        countStudents(process.argv[2])
+          .then((data) => res.end(data))
+          .catch((err) => res.end(err.message));
+        break;
+      default:
+        break;
     }
-  }
-  res.statusCode = 404;
-  res.end();
-});
-
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
-});
+  })
+  .listen(1245);
 
 module.exports = app;
